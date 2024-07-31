8051e0a5cd2def012e0292ea5804ac235013118a
Isaiah <zaymgmt707@gmail.com>
2024-07-30 21:09:18 -0700

    updates index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your code here
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad
+    this.soup = soup
+    this.drink = drink
+  }
+}
+// Creates a class for dinner and passes dessert as a private property
+class Dinner {
+  #dessert;
+  constructor(salad, soup, entree, dessert) {
+    this.salad = salad
+    this.soup = soup
+    this.entree = entree
+    this.#dessert = dessert
+  }
+}
